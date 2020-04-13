6959f4de48738bc5805a674f90474e383cc819e1
Vijay Joshi <vijayjoshi@macbook-pro-4.home>
2020-04-13 11:09:39 +0100

    struct sample usage

---
+++ tutorial/struct/main.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+type person struct {
+	firstName string
+	lastName  string
+}
+
+func main() {
+
+	var alex person
+	fmt.Println("alex: ", alex)
+	fmt.Printf("alex: %+v", alex)
+
+}
